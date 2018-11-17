743519483a028f89fa9a010c6ff484740b71f577
Iqra5545 <43742032+Iqra5545@users.noreply.github.com>
2018-11-17 11:15:01 -0800

    Donors

--- BootstrapSite2/BootstrapSite2/Controllers/PortfolioController.cs
+++ BootstrapSite2/BootstrapSite2/Controllers/PortfolioController.cs
@@ -8,38 +8,11 @@ namespace BootstrapSite2.Controllers
     public class PortfolioController : Controller
     {
 
-        public ActionResult OneColumn()
+        public ActionResult Donors()
         {
             return View();
         }
-        public ActionResult TwoColumn()
-        {
-            return View();
-        }
-        public ActionResult ThreeColumn()
-        {
-            return View();
-        }
-        public ActionResult FourColumn()
-        {
-            return View();
-        }
-        public ActionResult FiveColumn()
-        {
-            return View();
-        }
-        public ActionResult SixColumn()
-        {
-            return View();
-        }
-        public ActionResult SevenColumn()
-        {
-            return View();
-        }
-        public ActionResult EightColumn()
-        {
-            return View();
-        }
+       
         public ActionResult Details()
         {
             return View("Details");
